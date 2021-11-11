29ab0dde499b341220e63ab4fded197f13d964a8
mgrist <73457813+mgrist@users.noreply.github.com>
2021-11-10 18:09:09 -0600

    Fixed up appbar, added css stylesheet

--- client/src/components/appBar/appBar.js
+++ client/src/components/appBar/appBar.js
@@ -4,32 +4,20 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image'
-import 'bootstrap/dist/css/bootstrap.css';
 import Logo from '../../images/led2.png';
+import 'bootstrap/dist/css/bootstrap.css';
+import './appBar.css';
 
 export default function AppBar() {
-    // styling for PLED logo
-    const logo = {
-        height: '15%', 
-        width: '15%', 
-        paddingLeft: '20px',
-        paddingRight: '20px'
-    }
-    // styling for links, added padding on both sides
-    const links = {
-        paddingLeft: '15px',
-        paddingRight: '15px'
-    }
-
     return (
         <Navbar bg="dark" variant="dark">
         <Nav>
-            <Image src={Logo} href="#home" style={logo}/>
+            <Navbar.Brand id="logowrapper" href="#home"><Image className="logo" src={Logo}/></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav.Link style={links} href="#home">Work Orders</Nav.Link>
-            <Nav.Link style={links} href="#link">Report</Nav.Link>
-            <NavDropdown style={links} title="Settings" id="basic-nav-dropdown">
+            <Nav.Link className="links" href="#home">Work Orders</Nav.Link>
+            <Nav.Link className="links" href="#link">Report</Nav.Link>
+            <NavDropdown className="links" title="Settings" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
