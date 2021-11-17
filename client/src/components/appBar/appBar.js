601ed5de7c1fb8101f09bcce30f21894a0964c9e
julyjun <bag3401@gmail.com>
2021-11-17 08:40:54 -0600

    fixed link

--- client/src/components/appBar/appBar.js
+++ client/src/components/appBar/appBar.js
@@ -20,12 +20,12 @@ export default function AppBar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav.Link className="links" as={Link} to={"/WorkOrder"}>Work Orders</Nav.Link>
                 <Nav.Link className="links" as={Link} to={"/report"}>Report</Nav.Link>
-                <NavDropdown className="links" title="Settings" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown className="links" href="/action" title="Settings" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to={"action/3.1"}>Action</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to={"action/3.2"}>Another action</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to={"action/3.3"}>Something</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to={"action/3.4"}>Separated link</NavDropdown.Item>
                 </NavDropdown>
                 </Navbar.Collapse>
             </Nav>
@@ -37,6 +37,11 @@ export default function AppBar() {
                     <Route path="/home" element={<Home />} />
                     <Route path="/WorkOrder" element={<Workorder />} />
                     <Route path="/report" element={<Report />} />
+                    <Route path="action/3.1" element={<Home />} />
+                    <Route path="action/3.2" element={<Report />} />
+                    <Route path="action/3.3" element={<Report />} />
+
+                    <Route path="action/3.4" element={<Report />} />
                 </Routes>
             </div>
         </Router>
