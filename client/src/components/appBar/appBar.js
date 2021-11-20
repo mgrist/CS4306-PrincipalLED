0a21bd6e5a8f804da9e715dc39b696d1d2d0bb50
julyjun <bag3401@gmail.com>
2021-11-20 17:47:59 -0600

    added buttons in app bar

--- client/src/components/appBar/appBar.js
+++ client/src/components/appBar/appBar.js
@@ -1,17 +1,21 @@
-
 import React from 'react';
 import { Navbar, Nav, NavDropdown, Image } from 'react-bootstrap/';
 import Logo from '../../images/led2.png';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RefreshIcon from '@mui/icons-material/Refresh';
+
 import './appBar.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Workorder from '../pages/newOrder/newOrder';
 import Report from '../pages/Report/Report';
 import Home from    '../pages/Home';
 import Products from '../pages/Product/Product';
+import OrderForm from '../pages/newOrder/newOrder'
 import Stages from '../pages/stages/Stages.js';
 import DefactReason from '../pages/DefactReason/DefactReason';
 import Operator from '../pages/Operator/Operator';
+import ViewWorkOrder from '../pages/ViewWorkOrder/ViewWorkOrder';
 
 
 export default function AppBar() {
@@ -23,15 +27,31 @@ export default function AppBar() {
                 <Navbar.Brand as={Link} to={"/home"} id="logowrapper"><Image className="logo" src={Logo}/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav.Link className="links" as={Link} to={"/WorkOrder"}>View Orders</Nav.Link>
+                <Nav.Link className="links" as={Link} to={"/AddOrder"}>Add Orders</Nav.Link>
                 <Nav.Link className="links" as={Link} to={"/report"}>Report</Nav.Link>
                 <NavDropdown className="links" href="/Setting" title="Settings" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} className="drop-downs" to={"Setting/WorkOrder"}>Work Order</NavDropdown.Item>
                     <NavDropdown.Item as={Link} className="drop-downs" to={"Setting/Products"}>Products</NavDropdown.Item>
                     <NavDropdown.Item as={Link} className="drop-downs" to={"Setting/Stages"}>Stages</NavDropdown.Item>
                     <NavDropdown.Item as={Link} className="drop-downs" to={"Setting/Defact_Reason"}>Defect Reasons</NavDropdown.Item>
                     <NavDropdown.Item as={Link} className="drop-downs" to={"Setting/Operator"}>Operator</NavDropdown.Item>
+
                 </NavDropdown>
                 </Navbar.Collapse>
+
+                <Button  
+            color="secondary"
+            startIcon={<AddIcon />}
+            >
+            </Button>
+
+            <Button  
+            color="secondary"
+            sx={{ zIndex: 'tooltip' }}
+            startIcon={<RefreshIcon />}
+            >
+            </Button>
+
             </Nav>
             </Navbar>
             </div>
@@ -39,7 +59,8 @@ export default function AppBar() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/home" element={<Home />} />
-                    <Route path="/WorkOrder" element={<Workorder />} />
+                    <Route path="/AddOrder" element={<OrderForm />} />
+                    <Route path="Setting/WorkOrder" element={<ViewWorkOrder />} />
                     <Route path="/report" element={<Report />} />
                     <Route path="Setting/Products" element={<Products />} />
                     <Route path="Setting/Stages" element={<Stages />} />
