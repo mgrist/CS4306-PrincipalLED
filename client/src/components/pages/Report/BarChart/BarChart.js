5e8a2c053ed6f5c54c6a18be8c0b9f018c5d5c82
HungryGiant <91302996+HungryGiant@users.noreply.github.com>
2021-11-27 05:25:47 -0600

    chartbar.CSS(work in progress)

--- client/src/components/pages/Report/BarChart/BarChart.js
+++ client/src/components/pages/Report/BarChart/BarChart.js
@@ -7,7 +7,7 @@ import Chart from 'chart.js/auto'
 
 export default function BarChart() {
     return(
-      <div>
+      <div className="ChartBar">
         <Bar
         data= {{
           labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -33,8 +33,9 @@ export default function BarChart() {
               borderWidth: 1
           }]
       }}
-      height={40}
-      width={60}
+      height={30}
+      width={100}
+      
       />
       </div>
     )
